d3d073f3a57bb3e2668afaddd119132188fa2744
Galanx <feka.albert@gmail.com>
2018-08-10 11:31:16 +0200

    Removed dummy expense initialization

--- src/app.js
+++ src/app.js
@@ -10,10 +10,6 @@ import "./styles/styles.scss";
 
 const store = configStore();
 
-store.dispatch(addExpense({description: "Water bill", amount: 20, createdAt: 0}));
-store.dispatch(addExpense({description: "Gas bill", amount: 35, createdAt: 2}));
-store.dispatch(addExpense({description: "Internet bill", amount: 15, createdAt: 5}));
-console.log(React.version);
 const jsx = (
     <div>
         <Provider store={store}>
