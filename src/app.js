a7076fb649349b331205ccccc3089adc3d8f9ea3
Galanx <feka.albert@gmail.com>
2018-08-13 11:17:22 +0200

    setup production build and server

--- src/app.js
+++ src/app.js
@@ -3,8 +3,6 @@ import ReactDOM from "react-dom";
 import AppRouter from "./routes/AppRouter";
 import {Provider} from "react-redux";
 import configStore from "./store/configStore";
-import {addExpense} from "./actions/expenses";
-import {setFilterText} from "./actions/filters";
 import "normalize.css/normalize.css";
 import "./styles/styles.scss";
 
